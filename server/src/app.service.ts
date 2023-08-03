a45865f8175d39cd1cdfef84b8bc8fdf0e6e3784
DaniilBatiuk <sportak50@gmail.com>
2023-08-03 17:03:16 +0300

    Connect to db & test api

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import * as cheerio from 'cheerio';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private htmlString = `...`;
+
+  private extractWordsFromHTML(htmlString: string): string[] {
+    const words: string[] = [];
+    const cheerioInstance = cheerio.load(htmlString);
+
+    cheerioInstance('span.display-term').each((index, element) => {
+      const word = cheerioInstance(element).text();
+      words.push(word);
+    });
+
+    return words;
+  }
+
+  getHello(): string[] {
+    const words = this.extractWordsFromHTML(this.htmlString);
+    return words;
   }
 }
