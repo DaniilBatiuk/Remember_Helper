4b65696ea92131e4d7a879b82f5019eed917184c
DaniilBatiuk <sportak50@gmail.com>
2023-09-02 14:44:19 +0300

    update styles

--- client/src/components/Dictionary/Dictionary.tsx
+++ client/src/components/Dictionary/Dictionary.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import './Dictionary.scss';
 
 const Dictionary: React.FC = () => {
-  return <section className="dictionary"></section>;
+  return (
+    <section className="dictionary">
+      <h2 className="dictionary__title">English-Russian</h2>
+    </section>
+  );
 };
 export default Dictionary;
