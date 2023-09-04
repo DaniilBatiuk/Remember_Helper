aec492807795c6f27ff87ece555705ff85052535
DaniilBatiuk <sportak50@gmail.com>
2023-09-04 20:35:04 +0300

    update styles

--- client/src/components/Dictionary/Dictionary.tsx
+++ client/src/components/Dictionary/Dictionary.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import './Dictionary.scss';
+import Input from '../UI/Input/Input';
 
 const Dictionary: React.FC = () => {
   return (
     <section className="dictionary">
       <h2 className="dictionary__title">English-Russian</h2>
+      <form className="dictionary__form">
+        <Input type="text" placeholder="Search..." />
+        <button type="submit" className="dictionary__search">
+          Search
+        </button>
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+          <path
+            fill="currentColor"
+            d="M7 21q-.825 0-1.413-.588T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.588 1.413T17 21H7ZM17 6H7v13h10V6ZM9 17h2V8H9v9Zm4 0h2V8h-2v9ZM7 6v13V6Z"
+          />
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+          <path
+            fill="currentColor"
+            d="M5 21q-.825 0-1.413-.588T3 19V5q0-.825.588-1.413T5 3h4.2q.325-.9 1.088-1.45T12 1q.95 0 1.713.55T14.8 3H19q.825 0 1.413.588T21 5v6.7q-.475-.225-.975-.388T19 11.075V5H5v14h6.05q.075.55.238 1.05t.387.95H5Zm0-3v1V5v6.075V11v7Zm2-1h4.075q.075-.525.238-1.025t.362-.975H7v2Zm0-4h6.1q.8-.75 1.788-1.25T17 11.075V11H7v2Zm0-4h10V7H7v2Zm5-4.75q.325 0 .537-.213t.213-.537q0-.325-.213-.537T12 2.75q-.325 0-.537.213t-.213.537q0 .325.213.537T12 4.25ZM18 23q-2.075 0-3.538-1.463T13 18q0-2.075 1.463-3.538T18 13q2.075 0 3.538 1.463T23 18q0 2.075-1.463 3.538T18 23Zm-.5-2h1v-2.5H21v-1h-2.5V15h-1v2.5H15v1h2.5V21Z"
+          />
+        </svg>
+      </form>
     </section>
   );
 };
